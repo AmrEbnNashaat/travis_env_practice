992e893bfc259145b5e05aabe6e2259575549448
AmrEbnNashaat <slayerxd@live.com>
2023-02-01 18:40:05 +0200

    edited for excercise

---
+++ server.js
@@ -0,0 +1,14 @@
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+const favoriteFood = process.env.FAVORITE_FOOD;
+
+async function main() {
+  while(true) {
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
